13bb476480c8ec33eccca08c01803bfd2a14df75
Walter Breakell <walterjbreakell@gmail.com>
2024-05-03 03:18:46 -0700

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"runmates-529cd","appId":"1:730012581671:web:7b0978e85b2195d0e5b302","storageBucket":"runmates-529cd.appspot.com","apiKey":"AIzaSyDEfK9SOm0y8RfULU8axlRtxqlcXiNE6VM","authDomain":"runmates-529cd.firebaseapp.com","messagingSenderId":"730012581671","measurementId":"G-PRYEN8YJJR"}))), importProvidersFrom(provideAuth(() => getAuth()))]
 };
