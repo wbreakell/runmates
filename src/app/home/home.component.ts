c7413b92341e0ddd86f454d0b3c4d7eec681fda2
noah-kuo <31913344+noah-kuo@users.noreply.github.com>
2024-05-07 23:34:20 -0700

    Playing around with posts on the home screen.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+  Firestore,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -13,26 +21,48 @@ export class HomeComponent {
       id: 0,
       title: 'Post 0',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      pace: 0,
+      location: 'San Francisco',
     },
     {
       id: 1,
       title: 'Post 1',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      pace: 0,
+      location: 'San Francisco',
     },
     {
       id: 2,
       title: 'Post 2',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      pace: 0,
+      location: 'San Francisco',
     },
     {
       id: 3,
       title: 'Post 3',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      pace: 0,
+      location: 'San Francisco',
     },
     {
       id: 4,
       title: 'Post 4',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      pace: 0,
+      location: 'San Francisco',
     },
   ];
+
+  constructor(firestore: Firestore) {
+      const recentPostsQuery = query(collection(firestore, 'posts'), orderBy('timestamp', 'desc'), limit(12));
+
+      onSnapshot(recentPostsQuery, function (snapshot) {
+        snapshot.docChanges().forEach(function (change) {
+          var message = change.doc.data();
+          // Update the posts on the home page. But I have no idea what I'm doing so let's just print it out.
+          console.log(message);
+        });
+      });
+    }
 }
