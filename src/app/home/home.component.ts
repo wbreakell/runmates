c3a9886f4230e339927b2af435897d24916a9a92
noah-kuo <31913344+noah-kuo@users.noreply.github.com>
2024-05-08 20:22:20 -0700

    Update posts with firestore.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,52 +16,31 @@ import {
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  posts = [
-    {
-      id: 0,
-      title: 'Post 0',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      pace: 0,
-      location: 'San Francisco',
-    },
-    {
-      id: 1,
-      title: 'Post 1',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      pace: 0,
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      title: 'Post 2',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      pace: 0,
-      location: 'San Francisco',
-    },
-    {
-      id: 3,
-      title: 'Post 3',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      pace: 0,
-      location: 'San Francisco',
-    },
-    {
-      id: 4,
-      title: 'Post 4',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      pace: 0,
-      location: 'San Francisco',
-    },
-  ];
+  private _posts = [];
+
+  public get posts(): any {
+    return this._posts;
+  }
+
+  public set posts(val: any) {
+    this._posts = val;
+  }
 
   constructor(firestore: Firestore) {
       const recentPostsQuery = query(collection(firestore, 'posts'), orderBy('timestamp', 'desc'), limit(12));
-
-      onSnapshot(recentPostsQuery, function (snapshot) {
-        snapshot.docChanges().forEach(function (change) {
+      onSnapshot(recentPostsQuery, (snapshot) => {
+        this.posts = [];
+        snapshot.docChanges().forEach( (change) => {
           var message = change.doc.data();
-          // Update the posts on the home page. But I have no idea what I'm doing so let's just print it out.
-          console.log(message);
+          this.posts.push(
+            {
+              id: change.doc.id,
+              title: message['title'],
+              content: message['content'],
+              pace: message['pace'],
+              location: message['location'],
+            },
+          );
         });
       });
     }
