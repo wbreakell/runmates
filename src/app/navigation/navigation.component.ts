819827e4eab1acf8f84ccc09900d795912ac268e
noah-kuo <31913344+noah-kuo@users.noreply.github.com>
2024-05-09 23:32:15 -0700

    Add function to create fake posts.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  serverTimestamp,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-navigation',
@@ -8,4 +14,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  // Obviously we want to have some sort of pop up or page where the user
+  // can input all this stuff, but this is just a test for now.
+  async createPost() {
+    const docRef = await addDoc(collection(getFirestore(), 'posts'), {
+      author: "test2",
+      content: "This is another test",
+      location: "San Francisco",
+      pace: "Fast",
+      title: "Another test post",
+      timestamp: serverTimestamp(),
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
+}
