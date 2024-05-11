25b09b03c905d0263b919a9b5a5cce4d95427fab
Walter Breakell <walterjbreakell@gmail.com>
2024-05-11 03:50:47 -0700

    Create new-account component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,28 @@
 import { Routes } from '@angular/router';
+import { redirectUnauthorizedTo, AuthGuard } from '@angular/fire/auth-guard';
+
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+
+const redirectUnauthorizedToSignin = () => redirectUnauthorizedTo(['signin']);
 
 export const routes: Routes = [
   {
     path: '',
     title: 'Home · RunMates',
     component: HomeComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToSignin },
   },
   {
     path: 'signin',
-    title: 'Sign in · RunMates',
+    title: 'Sign In · RunMates',
     component: SigninComponent,
+  },
+  {
+    path: 'new-account',
+    title: 'Create Account · RunMates',
+    component: NewAccountComponent,
   },
 ];
