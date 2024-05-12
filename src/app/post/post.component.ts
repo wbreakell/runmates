1aba4c3e61306140edfe5d54468405d0fcf8fd8e
noah-kuo <31913344+noah-kuo@users.noreply.github.com>
2024-05-11 20:04:18 -0700

    Add ability to write a comment

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavigationComponent } from '../navigation/navigation.component';
 import {
   collection,
   query,
   orderBy,
-  limit,
   onSnapshot,
   Firestore,
+  addDoc,
+  getFirestore,
+  serverTimestamp,
 } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, ReactiveFormsModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
   private _comments = [];
+  private id = '';
   private timeOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  didIJustHitSubmitAndAmWaitingToLoad = false;
 
   public get comments(): any {
     return this._comments;
@@ -29,8 +34,14 @@ export class PostComponent {
     this._comments = val;
   }
 
+
+  commentForm = new FormGroup({
+    content: new FormControl('', Validators.required),
+  });
+
   constructor(firestore: Firestore, route: ActivatedRoute) {
-    const commentsQuery = query(collection(firestore, 'posts/' + route.snapshot.paramMap.get('id') +  '/comments'), orderBy('timestamp', 'asc'));
+    this.id = route.snapshot.paramMap.get('id')!;
+    const commentsQuery = query(collection(firestore, `posts/${this.id}/comments`), orderBy('timestamp', 'asc'));
     onSnapshot(commentsQuery, (snapshot) => {
       this.comments = [];
       snapshot.forEach((doc) => {
@@ -45,5 +56,19 @@ export class PostComponent {
         );
       });
     });
+  }
+
+  async comment() {
+    this.commentForm.disable();
+    this.didIJustHitSubmitAndAmWaitingToLoad = true;
+    await addDoc(collection(getFirestore(), `posts/${this.id}/comments`), {
+      author: "test2", // Get auth instead
+      content: this.commentForm.value.content!,
+      public: true,
+      timestamp: serverTimestamp(),
+    });
+    this.commentForm.enable();
+    this.commentForm.reset();
+    this.didIJustHitSubmitAndAmWaitingToLoad = false;
   }
 }
